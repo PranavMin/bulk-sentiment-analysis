2c792f894360d0a0e7caf728b0d71ce0908f6ce2
PranavMin <PranavMin@gmail.com>
2019-12-02 13:35:45 -0500

    Trying to add files

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 
 
 def main(URL="https://www.npr.org/2019/12/01/783989343/as-impeachment-inquiry-moves-to-judiciary-committee-republicans-attack-the-proce"):
+  print(URL)
   r = requests.get(URL)
   soup = BeautifulSoup(r.content, 'html5lib')
 
@@ -19,7 +20,15 @@ def main(URL="https://www.npr.org/2019/12/01/783989343/as-impeachment-inquiry-mo
   sentences_sentiment = [i.sentiment.polarity for i in blob.sentences]
   print(sum(sentences_sentiment)/len(sentences_sentiment))
 
+
 if len(sys.argv) > 1:
-  main(sys.argv[1])
+  if(sys.argv[1] == '-f'):
+    filename = sys.argv[2]
+    f = open(filename, 'r')
+    for line in f.readlines():
+      print(line)
+      main(URL=line)
+  else:
+    main(URL=sys.argv[1])
 else:
   main()
